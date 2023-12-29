f4a32148ca1e848b2429f53e0dcddc00968f9584
Julia-Kh <julia.khaipova@gmail.com>
2023-12-29 15:15:07 +0300

    Added Header

---
+++ src/goods.js
@@ -0,0 +1,32 @@
+export const goods = [
+    {
+        id: '1',
+        name: 'Robert L. Leahy, Beat the Blues Before They Beat You: How to Overcome Depression',
+        collection: 'Books',
+        owner: 'Julia Kha',
+    },
+    {
+        id: '2',
+        name: 'Leo Tolstoy, Anna Karenina',
+        collection: 'Books',
+        owner: 'Julia Kha',
+    },
+    {
+        id: '3',
+        name: 'Dante Alighieri, Divine Comedy',
+        collection: 'Books',
+        owner: 'Julia Kha',
+    },
+    {
+        id: '4',
+        name: 'Jean-Paul Sartre, Nausea',
+        collection: 'Books',
+        owner: 'Julia Kha',
+    },
+    {
+        id: '5',
+        name: 'Franz Kafka, The Trial',
+        collection: 'Books',
+        owner: 'Julia Kha',
+    },
+]
