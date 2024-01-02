4d024c480af9cbb7dff72bedd5b33711e38ae943
Julia-Kh <julia.khaipova@gmail.com>
2024-01-02 15:33:36 +0300

    setting Wordcloud

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from "react";
+import { useState } from 'react';
 // import './App.css'
-import PrimarySearchAppBar from "./components/Header";
+import PrimarySearchAppBar from './components/Header';
 import ItemsList from './components/ItemsList';
-import items from "./items";
-import the_biggest_collections from "./the_biggest_collections"
-import CollectionsList from "./components/CollectionsList"
+import items from './items';
+import the_biggest_collections from './the_biggest_collections';
+import CollectionsList from './components/CollectionsList';
+import Example from './components/Wordcloud';
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <PrimarySearchAppBar />
-            <ItemsList items={ items }></ItemsList>
-            <CollectionsList collections={ the_biggest_collections }></CollectionsList>
-        </>
-    );
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <ItemsList items={items}></ItemsList>
+      <CollectionsList collections={the_biggest_collections}></CollectionsList>
+      <Example width={400} height={400}></Example>
+    </>
+  );
 }
 
 export default App;
