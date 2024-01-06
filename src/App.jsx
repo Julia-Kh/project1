58e22bbf27ea1891c1a12e01f3cc1909f6e551b7
Julia-Kh <julia.khaipova@gmail.com>
2024-01-06 17:10:41 +0300

    the 5 biggest collections are loaded from the database now

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 // import './App.css'
 import PrimarySearchAppBar from './components/Header';
 import ItemsList from './components/ItemsList';
-import the_biggest_collections from './the_biggest_collections';
+// import the_biggest_collections from './the_biggest_collections';
 import CollectionsList from './components/CollectionsList';
 import Example from './components/Wordcloud';
 import { supabase } from './supabaseClient';
@@ -12,6 +12,7 @@ import { ThemeSupa } from '@supabase/auth-ui-shared';
 function App() {
   const [session, setSession] = useState(null);
   const [items, setItems] = useState([]);
+  const [collections, setCollections] = useState([]);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -26,6 +27,7 @@ function App() {
   }, []);
 
   useEffect(() => {
+    // get last 5 items
     supabase
       .from('Items')
       .select(
@@ -41,6 +43,23 @@ function App() {
           collection: value.Collections.title,
         }));
         setItems(data2);
+      });
+
+    // get 5 biggest collections
+    supabase
+      .from('Collections')
+      .select(`id, name:title, owner:author_id, poster:img_url, Items(count)`)
+      .then((res) => {
+        let { data, error } = res;
+        let arrOfData = data.map((value, key) => {
+          value.countOfItems = value.Items[0].count;
+          return value;
+        });
+        arrOfData.sort((a, b) => b.countOfItems - a.countOfItems);
+        arrOfData = arrOfData.slice(0, 5);
+
+        // set collections
+        setCollections(arrOfData);
       });
   }, []);
 
@@ -65,7 +84,7 @@ function App() {
       </div>
       <PrimarySearchAppBar />
       <ItemsList items={items}></ItemsList>
-      <CollectionsList collections={the_biggest_collections}></CollectionsList>
+      <CollectionsList collections={collections}></CollectionsList>
       <Example width={400} height={400}></Example>
     </>
   );
