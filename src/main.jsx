2a4d32e7575c0d393e0480e1a96c1470d773570e
Julia-Kh <julia.khaipova@gmail.com>
2024-01-09 21:19:49 +0300

    😑 It worked, but I have no idea what is going on here...

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
-import Root, { loader as rootLoader } from "./routes/root";
-import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from './routes/root';
+import ErrorPage from './error-page';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
 // import './index.css'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
+        path: 'contacts/:contactId',
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
