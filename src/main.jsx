2e2a0148b11365b67156bebde13634ea318e6659
Julia-Kh <julia.khaipova@gmail.com>
2024-01-22 15:58:36 +0300

    cleaning

--- src/main.jsx
+++ src/main.jsx
@@ -7,48 +7,11 @@ import {
   Route,
 } from 'react-router-dom';
 import App from './App.jsx';
-import Root, {
-  loader as rootLoader,
-  action as rootAction,
-} from './routes/root';
-import ErrorPage from './error-page';
-import Contact, {
-  loader as contactLoader,
-  action as contactAction,
-} from './routes/contact';
-import EditContact, { action as editAction } from './routes/edit';
-import './index2.css';
-import { action as destroyAction } from './routes/destroy';
-import Index from './routes/index';
-// import './index.css'
+
+import './index.css';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path="/"
-      element={<Root />}
-      loader={rootLoader}
-      action={rootAction}
-      errorElement={<ErrorPage />}
-    >
-      <Route errorElement={<ErrorPage />}>
-        <Route index element={<Index />} />
-        <Route
-          path="contacts/:contactId"
-          element={<Contact />}
-          loader={contactLoader}
-          action={contactAction}
-        />
-        <Route
-          path="contacts/:contactId/edit"
-          element={<EditContact />}
-          loader={contactLoader}
-          action={editAction}
-        />
-        <Route path="contacts/:contactId/destroy" action={destroyAction} />
-      </Route>
-    </Route>
-  )
+  createRoutesFromElements(<Route path="/" element={<App />} />)
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
