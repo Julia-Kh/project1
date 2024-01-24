5e10f1ea6013f87da13b552b3d799bed6a7d8ac5
Julia-Kh <julia.khaipova@gmail.com>
2024-01-24 17:26:04 +0300

    work on collection page

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -2,13 +2,13 @@ import Grid from '@mui/material/Grid';
 import Item from './Item';
 
 const ItemsList = ({ items }) => {
-    return (
-        <Grid container spacing={2}>
-            {items.map((item) => (
-                <Item key={item.id} {...item} />
-            ))}
-        </Grid>
-    )
-}
+  return (
+    <Grid container spacing={2}>
+      {items.map((item) => (
+        <Item key={item.id} {...item} />
+      ))}
+    </Grid>
+  );
+};
 
 export default ItemsList;
--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,8 +1,38 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect, useContext } from 'react';
+import ItemsList from '../components/ItemsList';
+import AuthContext from '../context/AuthContext';
 
 const CollectionPage = () => {
+  const { supabase } = useContext(AuthContext);
+  const [items, setItems] = useState([]);
   const { id } = useParams();
-  return <div>CollectionPage {id}</div>;
+  useEffect(() => {
+    // get items
+    supabase
+      .from('Items')
+      .select(
+        'id, name:title, Collections (title), created_at, owner:author_id, poster:img_url'
+      )
+      .eq('collection_id', id)
+      .order('created_at', { ascending: false }) // сортировка
+      .then((res) => {
+        let { data, error } = res;
+        // todo: handle errors
+        // console.log(data, error);
+        const data2 = data.map((value) => ({
+          ...value,
+          collection: value.Collections.title,
+        }));
+        setItems(data2);
+      });
+  }, []);
+  return (
+    <>
+      <div>Last Items</div>
+      <ItemsList items={items}></ItemsList>
+    </>
+  );
 };
 
 export default CollectionPage;
