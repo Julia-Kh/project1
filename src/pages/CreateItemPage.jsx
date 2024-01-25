a13e54ddc1d75f5f75df4c6cbd8a024a7a0408ce
Julia-Kh <julia.khaipova@gmail.com>
2024-01-25 16:13:59 +0300

    create item is working now

--- src/pages/CreateItemPage.jsx
+++ src/pages/CreateItemPage.jsx
@@ -1,7 +1,33 @@
-const CreateItemPage = () => {
-  return (
-    <div>CreateItemPage</div>
-  )
-}
+import React, { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
+import TypographyHeader from '../components/TypographyHeader';
+import FormItem from '../components/FormItem';
 
-export default CreateItemPage
+const CreateItemPage = () => {
+  const { supabase, session } = useContext(AuthContext);
+
+  const action = async (formData) => {
+    const { data, error } = await supabase
+      .from('Items')
+      .insert([
+        {
+          title: formData.title,
+          img_url: formData.imgUrl,
+          collection_id: formData.selectedValue,
+          description: formData.description,
+          author_id: session.user.id,
+        },
+      ])
+      .select(); // this is in documentation, I don't know why
+    return { data, error };
+  };
+
+  return (
+    <>
+      <TypographyHeader>Create item</TypographyHeader>
+      <FormItem action={action} />
+    </>
+  );
+};
+
+export default CreateItemPage;
